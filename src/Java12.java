cccd26d75af836a857d81b17e89905ac0f3dff0a
bienvu27 <bienvu27>
2023-02-20 11:42:30 +0700

    Java-12

---
+++ src/Java12.java
@@ -0,0 +1,11 @@
+public class Java12 {
+//    Cac phep toan mot ngoi
+    public static void main(String[] args) {
+        int a = 5;
+        boolean b = false;
+        System.out.println("-a => Gia tri: " + (-a));
+        System.out.println("+a => Gia tri: " + (+a));
+        System.out.println("!b => Gia tri: " + (!b));
+
+    }
+}
