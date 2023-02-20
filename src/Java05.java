d6ed9eb4d13c35f0a6f9181c357e05c39e6dadb0
bienvu27 <bienvu27>
2023-02-20 09:51:27 +0700

    Java-05

---
+++ src/Java05.java
@@ -0,0 +1,15 @@
+public class Java05 {
+//    Kieu du lieu trong java
+    public static void main(String[] args) {
+    String tenSach = "Lap trinh java";
+    int namXuatBan = 2021;
+    double giaTien = 15.5;
+    boolean conSach = true;
+    char maKho = 'M';
+    System.out.println("Ten sach: " + tenSach);
+    System.out.println("Nam: " + namXuatBan);
+    System.out.println("Gia: " + giaTien);
+    System.out.println("Trang thai: " + conSach);
+    System.out.println("Ma kho:" + maKho);
+    }
+}
